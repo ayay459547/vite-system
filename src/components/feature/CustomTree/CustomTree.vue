1f475cd49008832ea6d8446fa24cf93630ed582b
Caleb <ayay459547@gmil.com>
2023-11-22 18:06:53 +0800

    feat: CustomTree add expose

--- src/components/feature/CustomTree/CustomTree.vue
+++ src/components/feature/CustomTree/CustomTree.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ElTree } from 'element-plus'
-import { type PropType, useSlots } from 'vue'
+import { type PropType, useSlots, ref } from 'vue'
+import type Node from 'element-plus/es/components/tree/src/model/node'
 
 export interface TreeNodeData {
   [key: string]: any
@@ -59,11 +60,19 @@ const props = defineProps({
 })
 
 const emit = defineEmits([
-  'node-click'
+  'node-click',
+  'check-change',
+  'check'
 ])
 
 const onNodeClick = ($data1, $data2, $data3, $data4) => {
   emit('node-click', $data1, $data2, $data3, $data4)
+}
+const onCheckChange = ($data1, $data2, $data3) => {
+  emit('check-change', $data1, $data2, $data3)
+}
+const onCheck = ($data1, $data2) => {
+  emit('check', $data1, $data2)
 }
 
 // slot
@@ -72,11 +81,43 @@ const hasSlot = (prop: string): boolean => {
   return !!slots[prop]
 }
 
+const elTreeRef = ref<InstanceType<typeof ElTree>>()
+
+// 取得資料
+const getCheckedNodes = (leafOnly?: boolean, includeHalfChecked?: boolean) => {
+  return elTreeRef.value!.getCheckedNodes(leafOnly ?? false, includeHalfChecked ?? false)
+}
+const getCheckedKeys = (leafOnly?: boolean) => {
+  return elTreeRef.value!.getCheckedKeys(leafOnly ?? false)
+}
+
+// 設定勾選
+const setCheckedNodes = (nodeList: Node[], leafOnly?: boolean) => {
+  elTreeRef.value!.setCheckedNodes(nodeList, leafOnly ?? false)
+}
+
+const setCheckedKeys = (keyList: Array<string | number>, leafOnly?: boolean) => {
+  elTreeRef.value!.setCheckedKeys(keyList, leafOnly ?? false)
+}
+
+const resetChecked = () => {
+  elTreeRef.value!.setCheckedKeys([], false)
+}
+
+defineExpose({
+  getCheckedNodes,
+  getCheckedKeys,
+  setCheckedNodes,
+  setCheckedKeys,
+  resetChecked
+})
+
 </script>
 
 <template>
   <div class="tree-wrapper">
     <ElTree
+      ref="elTreeRef"
       :data="props.data"
       :props="props.props"
       :node-key="props.nodeKey"
@@ -84,6 +125,8 @@ const hasSlot = (prop: string): boolean => {
       :default-expand-all="props.defaultExpandAll"
       :show-checkbox="props.showCheckbox"
       @node-click="onNodeClick"
+      @check-change="onCheckChange"
+      @check="onCheck"
     >
       <!-- 資料為空 顯示內容 -->
       <template v-if="hasSlot('empty')" #empty>
--- src/views/Nav3-3-3/Nav3-3-3.vue
+++ src/views/Nav3-3-3/Nav3-3-3.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { ElTree } from 'element-plus'
-import { CustomTree, CustomDivider } from '@/components'
+import { CustomTree, CustomDivider, CustomButton } from '@/components'
+import { ref } from 'vue'
 
 interface Tree {
+  id: number
   label: string
   children?: Tree[]
 }
@@ -16,57 +18,50 @@ const handleNodeClick = (data: Tree, param, test, hello) => {
 
 const data: Tree[] = [
   {
+    id: 1,
     label: 'Level one 1',
     children: [
       {
+        id: 4,
         label: 'Level two 1-1',
         children: [
           {
+            id: 9,
             label: 'Level three 1-1-1'
+          },
+          {
+            id: 10,
+            label: 'Level three 1-1-2'
           }
         ]
       }
     ]
   },
   {
+    id: 2,
     label: 'Level one 2',
     children: [
       {
-        label: 'Level two 2-1',
-        children: [
-          {
-            label: 'Level three 2-1-1'
-          }
-        ]
+        id: 5,
+        label: 'Level two 2-1'
       },
       {
-        label: 'Level two 2-2',
-        children: [
-          {
-            label: 'Level three 2-2-1'
-          }
-        ]
+        id: 6,
+        label: 'Level two 2-2'
       }
     ]
   },
   {
+    id: 3,
     label: 'Level one 3',
     children: [
       {
-        label: 'Level two 3-1',
-        children: [
-          {
-            label: 'Level three 3-1-1'
-          }
-        ]
+        id: 7,
+        label: 'Level two 3-1'
       },
       {
-        label: 'Level two 3-2',
-        children: [
-          {
-            label: 'Level three 3-2-1'
-          }
-        ]
+        id: 8,
+        label: 'Level two 3-2'
       }
     ]
   }
@@ -76,21 +71,63 @@ const defaultProps = {
   children: 'children',
   label: 'label'
 }
+
+const treeRef = ref()
+
+const getCheckedNodes = () => {
+  console.log(treeRef.value!.getCheckedNodes(false, false))
+}
+const getCheckedKeys = () => {
+  console.log(treeRef.value!.getCheckedKeys(false))
+}
+
+const setCheckedNodes = () => {
+  const nodeList = [
+    {
+      id: 5,
+      label: 'Level two 2-1'
+    },
+    {
+      id: 9,
+      label: 'Level three 1-1-1'
+    }
+  ]
+  treeRef.value!.setCheckedNodes(nodeList, false)
+}
+
+const setCheckedKeys = () => {
+  const keyList = [3]
+  treeRef.value!.setCheckedKeys(keyList, false)
+}
+
+const resetChecked = () => {
+  treeRef.value!.setCheckedKeys([], false)
+}
 </script>
 
 <template>
   <div class="page">
-    <ElTree
-      :data="data"
-      :props="defaultProps"
-    />
+    <ElTree :data="data" :props="defaultProps" />
     <CustomDivider />
 
     <CustomTree
+      ref="treeRef"
       :data="data"
       :props="defaultProps"
+      show-checkbox
+      node-key="id"
+      default-expand-all
       @node-click="handleNodeClick"
     />
+
+    <div class="flex-row i-ga-sm">
+      <CustomButton type="info" plain label="get by node" @click="getCheckedNodes" />
+      <CustomButton type="primary" plain label="get by key" @click="getCheckedKeys" />
+      <CustomButton type="danger" plain label="set by node" @click="setCheckedNodes" />
+      <CustomButton type="warning" plain label="set by key" @click="setCheckedKeys" />
+      <CustomButton type="success" plain label="reset" @click="resetChecked" />
+    </div>
+    <CustomDivider />
   </div>
 </template>
 
