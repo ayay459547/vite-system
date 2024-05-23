f5e8497ab53c541cf2678f08cc26b45ba095c02b
Caleb <ayay459547@gmil.com>
2024-05-23 18:04:42 +0800

    feat: nav4-5

---
+++ src/views/Nav4-5/Nav4-5.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="page">4-5</div>
+</template>
+
+<style lang="scss" scoped>
+.page {
+  width: 100px;
+  height: 100px;
+  padding: 16px;
+}
+</style>
